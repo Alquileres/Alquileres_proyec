b5963a1ae611f381841c91d14380e36ddda94995
unknown <soledad110203@gmail.com>
2023-05-22 00:17:15 -0300

    agregue pagina (Publicar)su header y seccion

---
+++ index.js
@@ -0,0 +1,10 @@
+// const myDropdown = document.getElementById('myDropdown')
+// myDropdown.addEventListener('show.bs.dropdown', event => {
+//   // do something...
+// })
+let listElemens= document.querySelectorAll(".li-item-boton-clik");
+listElemens.forEach(listElemen=>{
+    Element.addEventlistener("clik",()=>{
+       alert()
+    });
+});
