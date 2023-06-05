bb3eb8e456c4852bdb4e73c492a6402dc9a40a54
milivillalba <soledad110203@gmail.com>
2023-06-05 16:46:00 -0300

    cambio de navegador-agregue cards al publicar

--- index.js
+++ index.js
@@ -2,9 +2,27 @@
 // myDropdown.addEventListener('show.bs.dropdown', event => {
 //   // do something...
 // })
-let listElemens= document.querySelectorAll(".li-item-boton-clik");
-listElemens.forEach(listElemen=>{
-    Element.addEventlistener("clik",()=>{
-       alert()
-    });
+// let listElemens= document.querySelector(".li-item-boton-clik");
+// listElemens.forEach(listElemen=>{
+//     Element.addEventlistener("clik",()=>{
+//        alert()
+//     });
+// });
+//btnCalcular.addEventListener('click', () => {
+
+//REGISTRATE
+const registrar_click = document.getElementById("regis_clik");
+registrar_click.addEventListener("clik", () => {
+  <ul class="dropdown-menu">
+    <li>
+      <a class="dropdown-item" href="#">
+        Como Locador
+      </a>
+    </li>
+    <li>
+      <a class="dropdown-item" href="#">
+        Como Locatario
+      </a>
+    </li>
+  </ul>;
 });
