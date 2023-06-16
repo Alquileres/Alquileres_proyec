270425b816fc0c25e8558b6c1720bd4133ecfe2e
unknown <soledad110203@gmail.com>
2023-06-16 03:00:37 -0300

    agregue algunas funcionalidades y conecte login y registro

--- index.js
+++ index.js
@@ -9,6 +9,18 @@
 //     });
 // });
 //btnCalcular.addEventListener('click', () => {
+    let cambiar_color = document.querySelector("body");
+    let cambiar_color_letras =document.querySelector(".tex-color");
+    let botom = document.querySelector(".checkbox");
+    
+    botom.addEventListener("click", updateName);
+    
+    function updateName() {
+      cambiar_color.classList.toggle("fondo-black");
+      cambiar_color_letras.classList.checkbox("tex-color");
+    }
+
+
 
 //REGISTRATE
 // const registrar_click = document.getElementById("regis_clik");
@@ -26,3 +38,25 @@
 //     </li>
 //   </ul>;
 // });
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    const carouselContainer = document.querySelector(".carousel-container");
+    const prevButton = document.querySelector(".carousel-control.prev");
+    const nextButton = document.querySelector(".carousel-control.next");
+    let currentIndex = 0;
+  
+    prevButton.addEventListener("click", function() {
+      if (currentIndex > 0) {
+        currentIndex--;
+        carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+      }
+    });
+  
+    nextButton.addEventListener("click", function() {
+      if (currentIndex < carouselContainer.children.length - 1) {
+        currentIndex++;
+        carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+      }
+    });
+  });
+  
