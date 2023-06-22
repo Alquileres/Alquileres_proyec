57b1b3c70be6c4a48920176411ba26815f0006f6
unknown <soledad110203@gmail.com>
2023-06-22 01:25:00 -0300

    cambios en la pag principal

--- index.js
+++ index.js
@@ -20,7 +20,46 @@
       cambiar_color_letras.classList.checkbox("tex-color");
     }
 
+//BOTON PUBLOC CARRUCEL
 
+// function cambiarContenedor() {
+//   var contenedor1 = document.getElementById('contenedor1');
+//   var contenedor2 = document.getElementById('contenedor2');
+//   var boton = document.getElementById('boton');
+  
+//   if (contenedor1.style.display !== 'none') {
+//     contenedor1.style.display = 'none';
+//     contenedor2.style.display = 'block';
+//     boton.innerHTML = 'Volver al Contenedor 1';
+//   } else {
+//     contenedor1.style.display = 'block';
+//     contenedor2.style.display = 'none';
+//     boton.innerHTML = 'Cambiar Contenedor';
+//   }
+// }
+
+
+function getCities() {
+  const cities = document.querySelectorAll(".sc-fifgRP");
+  const citiesNames = [];
+  for (const city of cities) {
+    citiesNames.push(city.getAttribute("name"));
+  }
+  return citiesNames;
+}
+
+
+
+
+
+// const buttons = document.querySelectorAll('.sc-sLsrZ hYtGoo span');
+// const container = document.querySelector('.sc-sLsrZ hYtGoo');
+
+// for (const button of buttons) {
+//   button.addEventListener('click', () => {
+//     container.classList.toggle('active');
+//   });
+// }
 
 //REGISTRATE
 // const registrar_click = document.getElementById("regis_clik");
